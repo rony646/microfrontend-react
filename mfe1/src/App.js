5401f1ece2745dc2419940f68b44e1a36a0eec68
Rony Silva <60190948+rony646@users.noreply.github.com>
2021-08-09 22:39:49 -0300

    Microfrontend 1 added

---
+++ mfe1/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from './Button';
+
+function App() {
+  return (
+    <div>
+      <h1>MFE1</h1>
+      <Button />
+    </div>
+  );
+}
+
+export default App;
---
+++ mfe1/src/Button.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Button  = () => (
+    <button style={{background: 'red'}}>MFE1 Button</button>
+);
+
+export default Button;
